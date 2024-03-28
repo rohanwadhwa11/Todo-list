34bf7c47dfb987f4bc2420232f1f0c837306482a
rohan wadhwa <rohanwadhwa913@outlook.com>
2024-03-28 13:59:36 +0530

    changed

--- todo app.js
+++ todo app.js
@@ -3,7 +3,26 @@ let btn = document.querySelector("button");
 let input = document.querySelector("input");
 let ul = document.querySelector("ul");
 
-btn.addEventListener("click",() => {
+btn.addEventListener("click",addElement);
+
+input.addEventListener("keypress",(e) => {
+     if(e.key == "Enter")
+     addElement();
+})
+
+ul.addEventListener("click",function(e) {
+    if(e.target.nodeName == "BUTTON"){
+        let listItem = e.target.parentElement;
+        listItem.style.textDecoration = "line-through";
+       setTimeout(() => {
+        listItem.remove();
+       }, 400);
+    console.log(listItem ,"deleted");
+    }
+})
+
+function addElement(){
+    if(input.value != ""){
     let item = document.createElement("li");
     item.innerText = input.value;
     ul.appendChild(item);
@@ -13,38 +32,12 @@ btn.addEventListener("click",() => {
     delbtn.classList.add("delete");
     item.appendChild(delbtn);
     input.value = "";
-    
-})
-
-input.addEventListener("keypress",(e) => {
-     if(e.key == "Enter")
-     {
-        let item = document.createElement("li");
-        item.innerText = input.value;
-        ul.appendChild(item);
-        let  delbtn = document.createElement("button");
-        delbtn.innerText = "✓";
-        delbtn.classList.add("delete");
-        item.appendChild(delbtn);
-        input.value = "";
-     }
-})
-
-ul.addEventListener("click",function(e) {
-    if(e.target.nodeName == "BUTTON"){
-        let listItem = e.target.parentElement;
-        listItem.remove();
-    console.log(listItem ,"deleted");
     }
-})
-
-/* let delbtns = document.querySelectorAll(".delete");
-console.log(delbtns);
-for(d of delbtns){
-    d.addEventListener("click",function() {
-        console.log(d);                                            //on console.log(d) why they only target last one
-        let par = this.parentElement;
-        par.remove();
-    })
+    else{
+        alert("enter a task to add it");
+        input.style.backgroundColor = "yellow";
+        setTimeout(() => {
+            input.style.backgroundColor = "lavender";
+        }, 500);
+    }
 }
- */
